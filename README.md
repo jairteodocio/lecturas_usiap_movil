5be52204b864dd41e962849d95af7d4c08109348
jairteodocio <jairerickiker@gmail.com>
2025-02-09 21:12:09 -0600

    inicio

---
+++ README.md
@@ -0,0 +1,3 @@
+# lecturas_usiap_movil
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/lecturas_usiap_movil/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lecturas_usiap_movil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
